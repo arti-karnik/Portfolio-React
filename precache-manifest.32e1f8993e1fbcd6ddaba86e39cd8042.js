b0f2c8513d257672278c431f85d72e7bd67994d7
Arti <arti.iosdev@gmail.com>
2021-05-24 11:30:13 -0700

    Updates

---
+++ precache-manifest.32e1f8993e1fbcd6ddaba86e39cd8042.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f5f1bdfae50a869669f01bb61c1a8b",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "4e061a5662c55961db33",
+    "url": "/Portfolio-React/static/css/2.dac0b718.chunk.css"
+  },
+  {
+    "revision": "034e803ee3114baf660d",
+    "url": "/Portfolio-React/static/css/main.38f1487a.chunk.css"
+  },
+  {
+    "revision": "4e061a5662c55961db33",
+    "url": "/Portfolio-React/static/js/2.7dc0c4f8.chunk.js"
+  },
+  {
+    "revision": "1733f615b5fb8951aa57be8986205fba",
+    "url": "/Portfolio-React/static/js/2.7dc0c4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034e803ee3114baf660d",
+    "url": "/Portfolio-React/static/js/main.d4536306.chunk.js"
+  },
+  {
+    "revision": "88f3eeda30bc80516240",
+    "url": "/Portfolio-React/static/js/runtime-main.4f639ff8.js"
+  },
+  {
+    "revision": "27d736848ac1966f06250c0e33c4162b",
+    "url": "/Portfolio-React/static/media/header-background.27d73684.jpeg"
+  }
+]);
