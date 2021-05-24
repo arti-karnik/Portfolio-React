48ed071b7a2f04b8e626da78d76fc194432cb1d8
Arti <arti.iosdev@gmail.com>
2021-05-24 11:17:16 -0700

    Updates

---
+++ precache-manifest.c4fcee033a7f5f9f8208c4931e2175ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f326dd4a3a36d3499cd160718dbe9c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e061a5662c55961db33",
+    "url": "/static/css/2.dac0b718.chunk.css"
+  },
+  {
+    "revision": "84e44356072d19606cda",
+    "url": "/static/css/main.4aed7945.chunk.css"
+  },
+  {
+    "revision": "4e061a5662c55961db33",
+    "url": "/static/js/2.7dc0c4f8.chunk.js"
+  },
+  {
+    "revision": "1733f615b5fb8951aa57be8986205fba",
+    "url": "/static/js/2.7dc0c4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e44356072d19606cda",
+    "url": "/static/js/main.db823228.chunk.js"
+  },
+  {
+    "revision": "dd0ad302ca727c93b192",
+    "url": "/static/js/runtime-main.4e7c19c6.js"
+  },
+  {
+    "revision": "27d736848ac1966f06250c0e33c4162b",
+    "url": "/static/media/header-background.27d73684.jpeg"
+  }
+]);
