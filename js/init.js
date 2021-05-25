bc59a870d0aeab9bdcfd4bea49e3246cbdac5a8b
Arti <arti.iosdev@gmail.com>
2021-05-25 03:23:54 -0700

    Updates

--- js/init.js
+++ js/init.js
@@ -26,7 +26,7 @@
 	    $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
+	       // 'scrollTop': $target.offset().top
 	    }, 800, 'swing', function () {
 	        window.location.hash = target;
 	    });
